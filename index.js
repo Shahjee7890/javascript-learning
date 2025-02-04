4004825ea6bc65f303cdca225ff1214fe0c57829
Syed Mujtaba Haider <zaidijr110@gmail.com>
2025-02-04 19:09:02 +0500

    foundation of js

---
+++ index.js
@@ -0,0 +1,234 @@
+//Date = 4/2/2025
+// ------------------------------------------
+// var _myName = 'shah';
+// var _1my__Name = 'shah';
+// var $myName = 'shah';
+
+// increment and decrement operator
+// --------------------------------------------
+// postfix operator
+
+// var num = 15;
+// var newNum = num++;
+// console.log(num);
+// console.log(newNum);
+
+// prefix operator
+
+// var num = 15;
+// var newNum = ++num;
+// console.log(num);
+// console.log(newNum);
+// ----------------------------------
+// decrease opreator
+
+// var num = 15;
+// var newNum = --num;
+// console.log(num);
+// console.log(newNum);
+// ----------------------------------
+// var num = 15;
+// var newNum = num--;
+// console.log(num);
+// console.log(newNum);
+// -------------------------
+// Comparision operator
+
+// var a = 30;
+// var b = 10;
+// --------------------------------------
+// equal operator (==)
+
+// console.log(a==b);
+
+// --------------------------------------
+
+// Not equal (!==)
+
+// console.log(a != b);
+
+// -------------------------------------
+
+// Greater than (>)
+
+// console.log(a>=b);
+// --------------------------------------
+
+// Greater than or equal (>=)
+
+// console.log(a>=b);
+// --------------------------------------
+
+// Less than (<)
+
+// console.log(a<b);
+
+// --------------------------------------
+
+// Less than Equal (<=)
+
+// console.log(a<=b);
+
+// --------------------------------------
+
+// Logical Operator
+
+// var a = 30;
+// var b = -20;
+
+// Logical and Operator (&&)
+// console.log(a > b && b > 0);
+
+// ----------------------------
+// Logical or Operator (||)
+// console.log(a > b || b > 0);
+
+// ----------------------------
+// Logical not operator (!)
+
+// console.log( !((a > b || b > 0)));
+// console.log(!false);
+
+// ---------------------------------
+
+// String Concatenation operator (+)
+
+// console.log("zaidi " + "shah");
+
+// var myName = "zaidi";
+// console.log(myName + " shah");
+
+// ----------------------------------------
+
+// Exponentiation operator (**)
+
+// console.log(2**2);
+
+// -------------------------------------------
+
+// console.log(10 ** -1);
+// -------------------------------------------
+// console.log(5 ="shah");
+
+// -------------------------------------------
+// var a = 5;
+// var b = 10;
+// var c = b;
+// b = a;
+// a = c;
+
+// console.log("value of a  " + a);
+// console.log("value of b  " + b);
+
+// -------------------------------------------
+
+// var a = 5;
+// var b = 10;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// console.log(a);
+// console.log(b);
+
+// -------------------------------------------
+
+// var num1 = 5;
+// var num2 = '5';
+
+// console.log(num1 == num2);
+
+// var num1 = 5;
+// var num2 = '5';
+
+// console.log(num1 === num2);
+
+// ------------------------------------------
+
+// if else
+
+// var tommorow = 'sunny';
+// if (tommorow === 'rain') {
+//     console.log('take a jakcet');
+
+// } else {
+//     console.log('koi zarurat nai hai ');
+
+// }
+
+// ------------------------------------------
+
+// IF else Task
+// --------------------------------
+
+// var year = 2020;
+
+// if (year % 4 === 0) {
+//   if (year % 100 === 0) {
+//     if (year % 400 === 0) {
+//       console.log("this year " + year + " is a leap year ");
+//     } else {
+//       console.log("this year " + year + " is not leap year ");
+//     }
+//   } else {
+//     console.log("this year " + year + " is a leap year ");
+//   }
+// } else {
+//   console.log("this year " + year + " is not leap year ");
+// }
+
+// ------------------------------------------
+
+// What is truthly and falsy balues in javaScript
+
+// we have total 5  falsy values in javaScript
+// 0 ,"" , undefine , null , NaN ,((( False is false anyway)))
+
+// EXAMPLE
+
+// if ((score = 1)) {
+//   console.log("no");
+// } else {
+//   console.log("yes");
+// }
+
+// ------------------------------------------
+
+// Conditional (ternary)  operator (:)
+
+// The Conditional (ternanry)  operator is only JavaScript operator that takes three operands
+// --------------------------------------------------------------------------------------------------
+
+// operands
+// 2 + 6
+
+// -----------------------------------------------------------------------------
+// Conditional (ternanry)  operator (Example)
+// var age = 18;
+// console.log(age >= 18 ? "yes" : "no");
+
+// --------------------------------
+
+// Switch Statement
+
+// var area = "circle";
+// var PI = 2.142,
+//   l = 5,
+//   b = 4,
+//   r = 3;
+
+// switch (area) {
+//   case "circle":
+//     console.log("the area of circle is : " + PI * r ** 2);
+//   case "triangle":
+//     console.log("the area of Triangle is : " + (l * b) / 2);
+//   case "circle":
+//     console.log("the area of circle is : " + l * b);
+
+//   default:
+//     console.log("please enter valid data");
+// }
+
+
+// --------------------------------------------------------------------------------------------------------
